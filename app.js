872d7dbf40fbf30dbebd8472a02b7aa93202bec8
AzizullahCodes <azizullahcodes@gmail.com>
2025-07-01 20:56:31 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,368 @@
+// problem-solving assignment no 37 
+// ===================================================================
+/*======== Question no 1 ==================
+create ul list and inside it li
+*/
+// ********** Answer********
+// let getDiv = document.getElementById('container');
+// let ul = document.createElement('ul');
+// let items = ['one','two','three'];
+// for(let i = 0; i < items.length; i++){let li = document.createElement('li');
+//   let liContent = document.createTextNode(items[i])
+// li.appendChild(liContent);
+// ul.appendChild(li)}
+// getDiv.appendChild(ul);
+
+/*======== Question no 2 ==================
+ul list creation by dom
+*/
+// ********** Answer********
+// let dv = document.getElementById('container');
+// console.log(dv);
+// let ul = document.createElement('ul');
+// console.log(ul);
+// let items = ['home','about','services','contacts','help'];
+
+// for(let i = 0; i < items.length; i++){let li = document.createElement('li');
+//   let liContent = document.createTextNode(items[i]);
+//   li.appendChild(liContent);
+//   ul.appendChild(li)
+// }
+// dv.appendChild(ul);
+/*======== Question no 3 ==================
+create a div by dom
+*/
+// ********** Answer********
+// let dv = document.createElement('div');
+// console.log(dv);
+// document.body.appendChild(dv);
+// dv.style.width = '100px';
+// dv.style.height = '200px';
+// dv.style.background = 'red';
+
+// let para = document.createElement('p');
+// let ptext = document.createTextNode('Hello javascript');
+// para.appendChild(ptext);
+// dv.appendChild(para)
+
+
+/*======== Question no 4 ==================
+How can i push multiple newcreatedElement in main div to show on DOM?
+*/
+// ********** Answer********
+// first we create main div 
+// let main = document.createElement('div');
+// console.log(main);
+// let heading = document.createElement('h1');
+// let headingContent = document.createTextNode('Pakistan');
+// heading.appendChild(headingContent);
+// console.log(heading);
+
+// let para = document.createElement('p');
+// let paraText = document.createTextNode('Pakistan is poor country');
+// para.appendChild(paraText);
+// console.log(para);
+// let spn = document.createElement('span');
+// let spnText = document.createTextNode('Hello');
+// spn.appendChild(spnText);
+// console.log(spn)
+
+// main.appendChild(heading);
+// main.appendChild(para);
+// main.appendChild(spn);
+
+// console.log(main);
+// document.body.appendChild(main)
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
+
+
+/*======== Question no 1 ==================
+
+*/
+// ********** Answer********
